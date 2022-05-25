7b8fe6189f717feac679b97c1a6c4946ac09111d
Marc Giroux <59919105+marcLeeroy@users.noreply.github.com>
2022-05-25 11:52:02 -0400

    testing installer

--- sail/Installer.php
+++ sail/Installer.php
@@ -2,10 +2,24 @@
 
 namespace SailCMS;
 
+use \Composer\Installer\PackageEvent;
+
 class Installer
 {
-    public static function runInstall()
+    public static function runInstall(PackageEvent $event)
     {
-        echo "Installing SailCMS v3.0...";
+        $package = $event->getOperation()->getPackage();
+        $installationManager = $event->getComposer()->getInstallationManager();
+
+        $originDir = $installationManager->getInstallPath($package);
+
+        if (file_exists($originDir) && is_dir($originDir)) {
+            mkdir("{$originDir}/sites");
+            mkdir("{$originDir}/sites/default");
+            mkdir("{$originDir}/public");
+            mkdir("{$originDir}/public/default");
+
+            copy(dirname(__DIR__) . '/cli', "{$originDir}/cli");
+        }
     }
 }
