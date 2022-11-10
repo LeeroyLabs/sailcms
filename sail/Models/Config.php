991189b790a9c09ee0c04177a73f8f20dcb1788b
Marc Giroux <59919105+marcLeeroy@users.noreply.github.com>
2022-11-10 11:06:17 -0500

    Added support for encrypted settings

--- sail/Models/Config.php
+++ sail/Models/Config.php
@@ -2,17 +2,22 @@
 
 namespace SailCMS\Models;
 
+use JsonException;
+use League\Flysystem\FilesystemException;
 use SailCMS\Database\BaseModel;
 use SailCMS\Errors\DatabaseException;
+use SailCMS\Security;
+use SodiumException;
 
 class Config extends BaseModel
 {
     public string $name;
     public array|object $config;
+    public bool $flag;
 
     public function fields(bool $fetchAllFields = false): array
     {
-        return ['name', 'config'];
+        return ['name', 'config', 'flag'];
     }
 
     /**
@@ -22,33 +27,53 @@ class Config extends BaseModel
      * @param  string  $name
      * @return static|null
      * @throws DatabaseException
+     * @throws JsonException
+     * @throws FilesystemException
+     * @throws SodiumException
      *
      */
     public static function getByName(string $name): ?static
     {
         $instance = new static();
-        return $instance->findOne(['name' => $name])->exec();
+        $item = $instance->findOne(['name' => $name])->exec();
+
+        if ($item && $item->flag) {
+            $item->config = json_decode(Security::decrypt($item->config), false, 512, JSON_THROW_ON_ERROR);
+        }
+
+        return $item;
     }
 
     /**
      *
-     * Store a setting to the config name
+     * Store a setting to the config name with optional encryption
      *
      * @param  string        $name
      * @param  array|object  $config
+     * @param  bool          $encrypt
      * @return void
      * @throws DatabaseException
+     * @throws FilesystemException
+     * @throws JsonException
+     * @throws SodiumException
      *
      */
-    public static function setByName(string $name, array|object $config): void
+    public static function setByName(string $name, array|object $config, bool $encrypt = false): void
     {
         $instance = new static();
         $record = static::getByName($name);
 
         if (!$record) {
+            $data = $config;
+
+            if ($encrypt) {
+                $data = Security::encrypt(json_encode($config, JSON_THROW_ON_ERROR));
+            }
+
             $instance->insert([
                 'name' => $name,
-                'config' => $config
+                'config' => $data,
+                'flag' => $encrypt
             ]);
             return;
         }
