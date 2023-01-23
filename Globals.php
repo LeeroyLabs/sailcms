d3fdc8a1f34da7501c368a5983ddbb27c8b45c07
Marc Giroux <59919105+marcLeeroy@users.noreply.github.com>
2023-01-23 08:58:54 -0500

    made env function conditional to laravel's env function not existing already

--- Globals.php
+++ Globals.php
@@ -1,17 +1,22 @@
 <?php
 
-/**
- *
- * Get a environment variable (set a default if not set)
- *
- * @param  string  $var
- * @param  mixed   $default
- * @return mixed
- *
- */
-function env(string $var, mixed $default = null): mixed
-{
-    return $_ENV[strtoupper($var)] ?? $default;
+// Add env function only if it does not exist already
+// This enables developers to use pieces of Laravel without
+// having a deadlock collision for this function
+if (!function_exists('env')) {
+    /**
+     *
+     * Get a environment variable (set a default if not set)
+     *
+     * @param  string  $var
+     * @param  mixed   $default
+     * @return mixed
+     *
+     */
+    function env(string $var, mixed $default = null): mixed
+    {
+        return $_ENV[strtoupper($var)] ?? $default;
+    }
 }
 
 /**
