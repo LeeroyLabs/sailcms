3ae576e0de1e1349661cf7441b52c32615aa223e
Marc Giroux <59919105+marcLeeroy@users.noreply.github.com>
2023-02-01 11:30:46 -0500

    clean up + v1 of GraphQL Client for unit testing graphql calls

---
+++ sail/Test/GraphQLClient.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace SailCMS\Test;
+
+use SailCMS\Test\Response\GraphQLResponse;
+
+class GraphQLClient
+{
+    public function __construct(private readonly string $url) { }
+
+    /**
+     *
+     * Execute a Query and get the return data
+     *
+     * @param  string  $query
+     * @param  array   $variables
+     * @param  string  $token
+     * @return object
+     * @throws \JsonException
+     *
+     */
+    public function run(string $query, array $variables, string $token = ''): object
+    {
+        $ch = curl_init();
+
+        $headers = [
+            'Content-Type: application/json',
+            'x-access-token: ' . $token
+        ];
+
+        $query = ['query' => $query, 'variables' => $variables];
+
+        curl_setopt($ch, CURLOPT_URL, $this->url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($query, JSON_THROW_ON_ERROR));
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+        $data = curl_exec($ch);
+        $error = curl_error($ch);
+        curl_close($ch);
+
+        if ($error) {
+            return new GraphQLResponse('failed', $error, null);
+        }
+
+        $json = json_decode($data, false, 512, JSON_THROW_ON_ERROR);
+        return new GraphQLResponse('ok', '', $json->data);
+    }
+}
