b7dfdc9526aa18b673a00cf37bd153258cb73133
Marc Giroux <59919105+marcLeeroy@users.noreply.github.com>
2023-04-12 15:36:50 -0400

    returned to old naming (breaks everything)

--- sail/Types/Pagination.php
+++ sail/Types/Pagination.php
@@ -4,5 +4,5 @@ namespace SailCMS\Types;
 
 class Pagination
 {
-    public function __construct(public readonly int $currentPage = 1, public readonly int $pageCount = 1, public readonly int $total = 0) { }
+    public function __construct(public readonly int $current = 1, public readonly int $totalPages = 1, public readonly int $total = 0) { }
 }
