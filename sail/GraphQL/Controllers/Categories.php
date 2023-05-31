d0f11c4ca57d889ce5002229b165fe194a93bf50
Marc Giroux <59919105+marcLeeroy@users.noreply.github.com>
2023-05-31 14:17:22 -0400

    fix(categories): changed order for tree

--- sail/GraphQL/Controllers/Categories.php
+++ sail/GraphQL/Controllers/Categories.php
@@ -150,7 +150,7 @@ class Categories
      */
     public function updateCategoryOrders(mixed $obj, Collection $args, Context $context): bool
     {
-        return (new Category())->updateOrder($args->get('order'), $args->get('site_id'));
+        return (new Category())->updateOrder($args->get('tree'), $args->get('site_id'));
     }
 
     /**
