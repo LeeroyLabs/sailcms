cab6d1834690c166c7c9293084a9e96d3f134d90
leeroy-phil <philippe@leeroy.ca>
2023-10-02 13:06:25 -0400

    chore: code optimization for getContent method

--- sail/Models/Entry.php
+++ sail/Models/Entry.php
@@ -376,10 +376,13 @@ class Entry extends Model implements Validator, Castable
             return $contentParsed;
         }
 
+        // To store matrix field keys
+        $matrixFieldKeys = [];
+
         // Get ids to fetch
-        $schema->each(function ($index, $fieldTab) use ($contentParsed, &$toFetch) {
+        $schema->each(function ($index, $fieldTab) use ($contentParsed, &$toFetch, &$matrixFieldKeys) {
             $fields =  $fieldTab->fields ? new Collection((array)$fieldTab->fields) : Collection::init();
-            $this->getIdToFetchFromContent($fields, $toFetch, $contentParsed);
+            $this->getIdToFetchFromContent($fields, $toFetch, $contentParsed, $matrixFieldKeys);
         });
 
         // Fetch data in batches
@@ -398,9 +401,9 @@ class Entry extends Model implements Validator, Castable
         }
 
         // Parse content with fetched elements
-        $contentParsed->each(function($key, &$content) use (&$contentParsed, $toFetch, $fetched) {
-            if (is_object($content) && !$content instanceof Collection) {
-                // It's not a collection, so it's a matrix content ~~~ todo be more precise - check in schema ?
+        $contentParsed->each(function($key, &$content) use (&$contentParsed, $toFetch, $fetched, $matrixFieldKeys) {
+            // Check in the matrix field keys array if it's a matrix field
+            if (in_array($key, $matrixFieldKeys)) {
                 $matrixContent = Collection::init();
                 foreach ($content as $matrixKey => $element) {
                     $contentFetched = $this->searchInFetchArray($key . "_" . $matrixKey, $element, $toFetch, $fetched);
@@ -424,6 +427,7 @@ class Entry extends Model implements Validator, Castable
      * @param Collection $fields
      * @param array $toFetch
      * @param Collection $content
+     * @param array $matrixFieldKeys
      * @param string $keyPrefix for matrix
      * @return void
      * @throws ACLException
@@ -431,7 +435,7 @@ class Entry extends Model implements Validator, Castable
      * @throws PermissionException
      *
      */
-    private function getIdToFetchFromContent(Collection $fields, array &$toFetch, Collection $content, string $keyPrefix = ""): void
+    private function getIdToFetchFromContent(Collection $fields, array &$toFetch, Collection $content, array &$matrixFieldKeys, string $keyPrefix = ""): void
     {
         foreach($fields as $entryField) {
             $elementBaseKey = $keyPrefix . $entryField->key;
@@ -454,7 +458,8 @@ class Entry extends Model implements Validator, Castable
             if ($entryField->type === "matrix") {
                 $subFields = (new EntryField)->getFieldsForMatrix($entryField->_id);
                 $matrixContent = new Collection((array)$content->get($entryField->key));
-                $this->getIdToFetchFromContent($subFields, $toFetch, $matrixContent, $elementBaseKey . "_");
+                $matrixFieldKeys[] = $entryField->key;
+                $this->getIdToFetchFromContent($subFields, $toFetch, $matrixContent, $matrixFieldKeys, $elementBaseKey . "_");
             } else if (isset($fetchKey) && isset($toFetch[$fetchKey]) && $content->get($entryField->key)) {
                 $fieldContent = $content->get($entryField->key);
                 if ($entryField->repeatable) {
